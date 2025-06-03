5296f49d9c38ba020cee165fac4f02f1e4e853e6
kiieeaa <kiyaadinda5@gmail.com>
2025-06-03 18:20:58 +0700

    lampu dapur

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -19,4 +19,13 @@ int main() {
     lampuRumah.setSaklarNo(0,"lampu teras rumah");
     lampuRumah.setSaklarNo(1, "lampu rumah tamu");
     lampuRumah.setSaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setSaklarNo(3, "lampu dapur");
+
+    cout << lampuRumah.getSatlarNo(0) << endl;
+    cout << lampuRumah.getSatlarNo(1) << endl;
+    cout << lampuRumah.getSatlarNo(2) << endl;
+    cout << lampuRumah.getSatlarNo(3) << endl;
+
+    return 0;
+    
 }
