79baa3a09bfa7e1d1c90fc33825dc6685d6a8e95
kiieeaa <kiyaadinda5@gmail.com>
2025-06-03 17:26:59 +0700

    pesan dari lia

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 class seseorang{
     public:
-    virtual void pesan() = 0
+    virtual void pesan() = 0;
 };
 
 class joko :public seseorang {
     public:
     void pesan() {
         cout << "pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+    public:
+    void pesan () {
+        cout << "pesan dari lia" << endl;
     }
 };
