ec1cc9bfc18194f05a152607187ae84d447df2e8
kiieeaa <kiyaadinda5@gmail.com>
2025-06-03 18:25:34 +0700

    class pekerja didalam virtual orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,3 +11,11 @@ class orang {
     }
 };
 
+class pekerja : virtual public orang {
+    public:
+    pekerja(int pUmur) :
+    orang(pUmur)
+    {
+        cout << " pekerja dibuat\n" << endl;
+    }
+};
