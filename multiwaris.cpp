f70df7359670ebe7c3913331a51f951cbce295bf
kiieeaa <kiyaadinda5@gmail.com>
2025-06-03 18:32:53 +0700

    hal ini dapat dilakukan jika menggunakan virtual

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,11 +11,37 @@ class orang {
     }
 };
 
-class pekerja : virtual public orang {
-    public:
-    pekerja(int pUmur) :
-    orang(pUmur)
-    {
-        cout << " pekerja dibuat\n" << endl;
+class pekerja :virtual public orang{
+    public: 
+    pekerja (int pUmur):
+    orang(pUmur){
+        cout << "Pekerja dibuat\n" << endl;
+
     }
 };
+class pelajar : virtual public orang {
+    public:
+    pelajar(int pUmur) :
+    orang(pUmur)
+    {
+        cout << " pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+    public:
+
+    budi(int pUmur) :
+    pekerja (pUmur),
+    pelajar(pUmur),
+    orang(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+
+};
+
+int main() {
+    budi a(12);
+    return 0;
+}
